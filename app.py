4620e772648d843aec013ee33e98b9bcc889d280
Pavankalyan V <162774471+Pavan23223@users.noreply.github.com>
2026-04-23 19:07:41 +0530

    Remove emojis from subheaders in app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ import pandas as pd
 
 df = pd.read_csv("attendance.csv")
 
-st.title("📊 Smart Attendance Dashboard")
+st.title("Smart Attendance Dashboard")
 
 st.dataframe(df)
 
-st.subheader("📈 Duration per Student")
+st.subheader("Duration per Student")
 st.bar_chart(df.groupby("Name")["Duration_Minutes"].sum())
 
-st.subheader("⚠️ Late Students")
+st.subheader(" Late Students")
 late_df = df[df["Late"] == "Yes"]
 st.dataframe(late_df)
 
-st.subheader("📊 Attendance Status")
+st.subheader(" Attendance Status")
 st.bar_chart(df["Status"].value_counts())
